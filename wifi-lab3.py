8b89ca48d20a687695c37d5390f593b4855a9d8c
Adele <adele.therias@live.com>
2021-12-14 10:50:53 +0100

    added plotting

--- wifi-lab3.py
+++ wifi-lab3.py
@@ -1,5 +1,7 @@
 import csv
 import os
+import pandas as pd
+import matplotlib.pyplot as plt
 
 def parse_files(input_file):
     #Converts .txt file output by Homedale app into .csv for analysis
@@ -26,3 +28,33 @@ for filename in os.listdir("C:/Users/Adele/Documents/TU-Delft/Q2/GEO1003 - Geopo
         parse_files(filename)
     else:
         continue
+
+def identify_location(input_file):
+    # read CSV file (note: first row considered the header, so it starts from row 2)
+    mystery_location = pd.read_csv(input_file,sep=",",index_col = False, usecols=["mac","signal_strength"])
+    mystery_mac_mean_signal = mystery_location.groupby('mac').mean().sort_values('mac')
+    # ax1 = mystery_mac_mean_signal.plot(mystery_mac_mean_signal)
+    # plt.show()
+
+    # top_10_mac_mean = mac_mean_signal.sort_values('signal_strength').head(10)
+    for filename in os.listdir("C:/Users/Adele/Documents/TU-Delft/Q2/GEO1003 - Geopositioning/Lab 3 - Wifi Fingerprinting/Geopositioning"):
+        if filename.startswith("parsed") and filename.endswith(".csv"):
+            reference_location = pd.read_csv(filename, sep=",", index_col=False, usecols=["mac", "signal_strength"])
+            reference_mac_mean_signal = reference_location.groupby('mac').mean().sort_values('mac')
+            plt_reference = ax2.plt.plot(reference_mac_mean_signal)
+            fig,(ax1,ax2) = plt.subplots(2)
+            ax1.plot(mystery_mac_mean_signal)
+            ax2.plot(reference_mac_mean_signal)
+            plt.show()
+            # fig, (plt_mystery, plt_reference) = plt.subplots(2)
+            # plt.show()
+        else:
+            continue
+
+identify_location("parsed_measurement16-group4.csv")
+
+# for filename in os.listdir("C:/Users/Adele/Documents/TU-Delft/Q2/GEO1003 - Geopositioning/Lab 3 - Wifi Fingerprinting/Geopositioning"):
+#     if filename.startswith("parsed") and filename.endswith(".csv"):
+#         identify_location(filename)
+#     else:
+#         continue
